389c7d42d76ca240b1bba347cbaf14454a2b0666
gimin <gimin@outlook.com>
2017-01-18 18:59:04 +0200

    final

--- hanoiTowers/Algorithm.java
+++ hanoiTowers/Algorithm.java
@@ -4,14 +4,14 @@ package hanoiTowers;
  * Created by gimin on 1/17/17.
  */
 public class Algorithm {
-    public int solveHanoiTowers(int n, char first, char middle, char last) {
+    public int solveHanoiTowers(int n, char source, char destination, char auxilliary) {
         int count = 0;
 
         if (n > 0) { // do 2 recursive calls
-            count = solveHanoiTowers(n - 1, first, last, middle);
-            System.out.println("Plate " + n + " from " + first + " rod, to " + last + " rod.");
+            count = solveHanoiTowers(n - 1, source, auxilliary, destination);
+            System.out.println("Plate " + n + " from " + source + " rod, to " + auxilliary + " rod.");
             count++;
-            count += solveHanoiTowers(n - 1, middle, first, last);
+            count += solveHanoiTowers(n - 1, destination, source, auxilliary);
         }
         return count;
     }
